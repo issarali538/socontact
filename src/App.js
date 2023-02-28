3ab458ceb8c28cd6293f4c456c4ad8150c477d0f
issarali538 <you@example.com>
2023-03-01 00:33:35 +0500

    commit 28 feb

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import path from 'path';
 import './assets/css/Style.css';
-import {createContext, useState} from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Button, Modal} from 'react-bootstrap';
+import { createContext, useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Config/Header';
 import Aside from './Config/Aside';
 import MainCont from './MainCont';
@@ -20,33 +21,58 @@ import MemberList from './Comps/MemberList/MemberList';
 import AddTaskMain from './Comps/AddTask/Main';
 
 const App = () => {
-  
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  useEffect(() =>handleShow, []);
+
   return (
-    <div id="page-wrapper">
-      <BrowserRouter>
-        <Header />
-        <Aside />
-        <MainCont>
-          <div className='page-content'>
-            <Routes>
-              <Route path="/" exact element={<Dashboard />} />
-              <Route path="/services"  exact element={<Services />} />
-              <Route path="/package" exact element={<Package />} />
-              <Route path="/leads" exact element={<Leads />} />
-              <Route path="/profile" exact element={<Profile />} />
-              <Route path="/team" exact element={<Team />} />
-              <Route path="/setting" exact element={<Setting />} />
-              <Route path="/lead1" exact element={<Lead1 />} />
-              <Route path="/lead2" exact element={<Lead2 />} />
-              <Route path="/billing" exact element={<Billing />} />
-              <Route path="/member-list" exact element={<MemberList />} />
-             
-            </Routes>
-          </div>
-        </MainCont>
-      </BrowserRouter>
-    </div>
-    
+    <>
+      <div id="page-wrapper">
+        <BrowserRouter>
+          <Header />
+          <Aside />
+          <MainCont>
+            <div className='page-content'>
+              <Routes>
+                <Route path="/" exact element={<Dashboard />} />
+                <Route path="/services" exact element={<Services />} />
+                <Route path="/package" exact element={<Package />} />
+                <Route path="/leads" exact element={<Leads />} />
+                <Route path="/profile" exact element={<Profile />} />
+                <Route path="/team" exact element={<Team />} />
+                <Route path="/setting" exact element={<Setting />} />
+                <Route path="/lead1" exact element={<Lead1 />} />
+                <Route path="/lead2" exact element={<Lead2 />} />
+                <Route path="/billing" exact element={<Billing />} />
+                <Route path="/member-list" exact element={<MemberList />} />
+
+              </Routes>
+            </div>
+          </MainCont>
+        </BrowserRouter>
+      </div>
+
+
+      <Modal show={show} onHide={handleClose} centered size="lg">
+          <Modal.Header closeButton>
+            <Modal.Title className='fw-bold'>Please!</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+              <h5 className='fw-bold'>
+                <h3 className='text-center text-success'> <i className="fa-solid fa-circle-exclamation"></i> </h3>
+                This Product is just for to show my work, so this is not proper functional. 
+
+              </h5>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="success" onClick={handleClose}>OK</Button>
+          </Modal.Footer>
+        </Modal>
+
+    </>
   );
 }
 
